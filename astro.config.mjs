d9b4db778294fde778b1907a5c0f1c85074aac81
liufeng <13710741681@163.com>
2026-02-04 17:54:39 +0800

    set site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://warm-haupia-9c9aa2.netlify.app",
   integrations: [sitemap()],
 });
